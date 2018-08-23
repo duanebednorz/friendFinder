602d5e0c4cac70da4336853b49f20eb73a54dd74
Duane B <duanebednorz@aggienetwork.com>
2018-08-22 23:09:42 -0500

    fixed paths

---
+++ app/data/friends.js
@@ -0,0 +1,165 @@
+// Data structure that will store the friends data
+var friendsArray = [
+	{
+		name: 'Kim Kardashian',
+		photo: 'https://pbs.twimg.com/profile_images/994943919371706368/QTyfb7-M_400x400.jpg',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Barack Obama',
+		photo: 'https://consequenceofsound.files.wordpress.com/2016/08/obama-summer-playlist.png?w=807',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Justin Bieber',
+		photo: 'https://media.glamour.com/photos/56e1f368bebf143c52613a88/master/w_1280,c_limit/entertainment-2016-02-justin-bieber-GQ-black-and-white-main.jpg',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Rihanna',
+		photo: 'https://i1.wp.com/beginningandend.com/wp-content/uploads/2012/04/rihanna-rated-r-devil-horns-Illuminati-e1333713741578.png?resize=500%2C511&ssl=1',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Donald Trump',
+		photo: 'https://fortunedotcom.files.wordpress.com/2017/08/donald-trump1.jpg',
+		scores: [ 
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Taylor Swift',
+		photo: 'https://pixel.nymag.com/imgs/daily/vulture/2017/11/09/09-taylor-swift-ranking-feature.nocrop.w1600.h2147483647.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Lionel Messi',
+		photo: 'https://thenypost.files.wordpress.com/2017/07/lionelmessi.jpg?quality=90&strip=all&w=618&h=410&crop=1',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Vladimir Putin',
+		photo: 'http://ste.india.com/sites/default/files/2018/03/18/669398-final-putin-2.jpg',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Mark Zuckerberg',
+		photo: 'https://fortunedotcom.files.wordpress.com/2016/12/mark-zuckerberg-ap_16356479069476.jpg',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Ryan Reynolds',
+		photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRfY4sXfkXzFEmhaxSfbRHmQZQk57D-e_kQI27N7Qxb4WJKFbm7A',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsArray;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,38 @@
+var friendsList = require("../data/friends");
+
+module.exports = function(app){
+	app.get("/api/friends", function(req, res){
+		res.json(friendsList);
+		
+	})
+
+// Create New Characters - takes in JSON input
+app.post("/api/friends", function(req, res) {
+	var newFriend = req.body;
+	var newScore = 0;
+	var total = 0;
+	var match = {
+		name: "",
+		photo: "",
+		scores: 10000
+	}
+	console.log(friendsList);
+	// Calculating totals 
+	for (var i = 0; i < friendsList.length; i++) {
+		total = 0;
+
+		for (var j = 0; j < friendsList[i].scores.length; j++) {
+			total += Math.abs(friendsList[i].scores[j] - newFriend.scores[j]);
+
+			if (total <= match.difference) {
+				match.name = friendsList[i].name,
+				match.photo = friendsList[i].photo,
+				match.difference = total
+			}
+    	}
+    }
+    friendsList.push(newFriend);
+    res.json(match);
+    console.log(match);
+});
+}
